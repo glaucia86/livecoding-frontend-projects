139608806a613a92ec78d15f79a7523fb2b1a2df
glaucia86 <glaucia_lemos86@hotmail.com>
2020-01-01 17:45:33 -0300

    Alteração no arquivo 'main.js' - configuração inicial no arquivo principal de execucao do projeto.

--- project-1/chat-app-vue/src/main.js
+++ project-1/chat-app-vue/src/main.js
@@ -1,9 +1,22 @@
+// @ts-nocheck
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import VueChatScroll from 'vue-chat-scroll';
+import 'bootstrap';
+
 import App from './App.vue';
-import router from './router';
-import store from './store';
+import router from './router/index';
+import store from './store/index';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import './assets/css/loading.css';
+import './assets/css/loading-btn.css';
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+Vue.use(VueChatScroll);
 
 new Vue({
   router,
