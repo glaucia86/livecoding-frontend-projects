5a956c018048fd15bb6dc7ba3ce351d774f64900
glaucia86 <glaucia_lemos86@hotmail.com>
2020-01-01 18:00:45 -0300

    src/store/index.js - Configuração e inclusão do plugin: VuexPersistence

--- project-1/chat-app-vue/src/store/index.js
+++ project-1/chat-app-vue/src/store/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist';
+
+import mutations from './mutations';
+import actions from './actions';
 
 Vue.use(Vuex);
+
+const debug = process.env.NODE_ENV !== 'production';
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+});
 
 export default new Vuex.Store({
   state: {
   },
-  mutations: {
+  mutations,
+  actions,
+  getters: {
   },
-  actions: {
-  },
-  modules: {
-  },
+  plugins: [vuexLocal.plugin],
+  strict: debug,
 });
