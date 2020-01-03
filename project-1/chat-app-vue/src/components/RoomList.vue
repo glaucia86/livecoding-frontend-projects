98e841dbcfb180e250df6940ea9fc3dc5be464b4
glaucia86 <glaucia_lemos86@hotmail.com>
2020-01-03 19:36:07 -0300

    Desenvolvimento do componente - 'RoomList.vue'

---
+++ project-1/chat-app-vue/src/components/RoomList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="room-list">
+    <h4>Canais</h4>
+    <hr>
+    <b-list-group v-if="activeRoom">
+      <b-list-group-item v-for="room in rooms"
+                          :key="room.name"
+                          :active="activeRoom.id === room.id"
+                          href="#"
+                          @click="onChange(room)">
+        # {{ room.name }}
+      </b-list-group-item>
+    </b-list-group>
+  </div>
+</template>
+
+<script>
+import mapState from 'vuex';
+
+export default {
+  name: 'RoomList',
+
+  computed: {
+    ...mapState([
+      'activeRoom',
+      'rooms',
+    ]),
+  },
+};
+</script>
