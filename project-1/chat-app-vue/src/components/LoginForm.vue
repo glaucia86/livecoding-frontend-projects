e07c34d11fb42c16fb319c26f766975a208e84f4
glaucia86 <glaucia_lemos86@hotmail.com>
2020-01-02 23:38:01 -0300

    Desenvolvimento do componente de 'LoginForm'

---
+++ project-1/chat-app-vue/src/components/LoginForm.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="login-form">
+      <h5 class="text-center">Chat Login</h5>
+      <hr>
+      <b-form @submit.prevent="onSubmit">
+
+        <b-alert variant="danger" :show="hasError">{{ error }}</b-alert>
+        <b-form-group id="userInputGroup" label="userInput" label-for="userInput">
+          <b-form-input id="userInput"
+                        type="text"
+                        placeholder="Digite o seu usuário"
+                        v-model="userId"
+                        autocomplete="off"
+                        :disabled="loading"
+                        required>
+          </b-form-input>
+        </b-form-group>
+
+        <b-button type="submit"
+                  variant="primary"
+                  class="ld-ext-right"
+                  v-bind:class="{ running: loading }"
+                  :disabled="isValid">
+                  Login <div class="ld ld-ring ld-spin"></div>
+        </b-button>
+      </b-form>
+    </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex';
+
+export default {
+  name: 'login-form',
+  data() {
+    return {
+      userId: '',
+    };
+  },
+  computed: {
+    // Método responsável por validar o nome de usuário.
+    isValid() {
+      // Ex.: user: glaucia_lemos86 (userId => resultado)
+      const resultado = this.userId.length < 3;
+      return resultado || this.loading;
+    },
+    ...mapState([
+      'loading',
+      'error',
+    ]),
+    ...mapGetters([
+      'hasError',
+    ]),
+  },
+};
+</script>
+
+<style lang="stylus" scoped></style>
