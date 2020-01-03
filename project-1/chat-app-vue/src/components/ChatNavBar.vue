463a4d650dd49ecfe999ab738c7e97fe9d8c7442
glaucia86 <glaucia_lemos86@hotmail.com>
2020-01-03 19:21:38 -0300

    Desenvolvimento do navbar - 'ChatNavBar.vue'

---
+++ project-1/chat-app-vue/src/components/ChatNavBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-navbar id="chat-navbar" toggleable="md" type="dark" variant="primary">
+    <b-navbar-brand href="#">
+      Aplicação Chat com Vue
+    </b-navbar-brand>
+    <b-navbar-nav class="ml-auto">
+      <b-nav-text>{{ user.name }} | </b-nav-text>
+      <b-nav-item href="#" active>Logout</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+import mapState from 'vuex';
+
+export default {
+  name: 'ChatNavBar',
+
+  computed: {
+    ...mapState([
+      'user',
+    ]),
+  },
+};
+
+</script>
