ef8850d5d41de49104f5a797a9f93b17719998aa
glaucia86 <glaucia_lemos86@hotmail.com>
2020-01-14 23:27:41 -0300

    Desenvolvimento do arquivo 'mutation.js'

---
+++ project-1/chat-app-vue/src/chatkit.js
@@ -0,0 +1,30 @@
+/**
+ * arquivo: chatkit.js
+ * Data: 14/01/2020
+ * Descrição: arquivo responsável por tratar a lógica da autenticação entre a aplicação e o ChatKit
+ * Author: Glaucia Lemos
+ */
+
+import { ChatManager, TokenProvider } from '@pusher/chatkit-client';
+
+const INSTANCE_LOCATOR = process.env.VUE_CHAT_APP_INSTANCE_LOCATOR;
+const TOKEN_URL = process.env.VUE_CHAT_APP_TOKEN_URL;
+const MESSAGE_LIMIT = Number(process.env.VUE_CHAT_APP_MESSAGE_LIMIT) || 10;
+
+let currentUser = null;
+let activeRoom = null;
+
+async function connectUser(userId) {
+  const chatManager = new ChatManager({
+    instanceLocator: INSTANCE_LOCATOR,
+    tokenProvider: new TokenProvider({ url: TOKEN_URL }),
+    userId,
+  });
+
+  currentUser = await chatManager.connect();
+  return currentUser;
+}
+
+export default {
+  connectUser,
+};
---
+++ project-1/chat-app-vue/src/store/mutations.js
@@ -0,0 +1,65 @@
+/**
+ * arquivo: src/store/mutations.js
+ * Data: 14/01/2020
+ * Descrição
+ * Author: Glaucia Lemos
+ */
+
+export default {
+  setError(state, error) {
+    state.error = error;
+  },
+
+  setLoading(state, loading) {
+    state.loading = loading;
+  },
+
+  setUser(state, user) {
+    state.user = user;
+  },
+
+  setReconnect(state, reconnect) {
+    state.reconnect = reconnect;
+  },
+
+  setActiveRoom(state, roomId) {
+    state.activeRoom = roomId;
+  },
+
+  setRooms(state, rooms) {
+    state.rooms = rooms;
+  },
+
+  setUsers(state, users) {
+    state.users = users;
+  },
+
+  clearChatRoom(state) {
+    state.users = [];
+    state.messges = [];
+  },
+
+  setMessages(state, messages) {
+    state.messages = messages;
+  },
+
+  addMessage(state, message) {
+    state.message.push(message);
+  },
+
+  setSending(state, status) {
+    state.sending = status;
+  },
+
+  setUserTyping(state, userId) {
+    state.userTyping = userId;
+  },
+
+  reset(state) {
+    state.error = null;
+    state.users = [];
+    state.messages = [];
+    state.rooms = [];
+    state.user = null;
+  },
+};
