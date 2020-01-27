4dffc2cab68cbc85acda6f6e62299b54ad69ad74
glaucia86 <glaucia_lemos86@hotmail.com>
2020-01-26 23:45:20 -0300

    Desenvolvimento da lógica para tratar renderização da aplicação - 'ChatNavBar.vue'

--- project-1/chat-app-vue/src/components/ChatNavBar.vue
+++ project-1/chat-app-vue/src/components/ChatNavBar.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState, mapActions, mapMutations } from 'vuex';
 
 export default {
   name: 'ChatNavBar',
@@ -19,7 +19,34 @@ export default {
   computed: {
     ...mapState([
       'user',
+      'reconnect',
     ]),
+  },
+
+  methods: {
+    ...mapActions([
+      'logout',
+      'login',
+    ]),
+    ...mapMutations([
+      'setReconnect',
+    ]),
+    onLogout() {
+      this.$router.push({ path: '/' });
+      this.logout();
+    },
+    unload() {
+      if (this.user.username) {
+        this.setReconnect(true);
+      }
+    },
+  },
+
+  mounted() {
+    window.addEventListener('beforeunload', this.unload);
+    if (this.reconnect) {
+      this.login(this.user.username);
+    }
   },
 };
 </script>
