f3025656055572753b6266f8162516c849ce1a9c
glaucia86 <glaucia_lemos86@hotmail.com>
2020-02-22 01:06:54 -0300

    criando arquivo de serviço.

--- project-2/weather-app-angular/src/app/app.module.ts
+++ project-2/weather-app-angular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
@@ -15,6 +16,7 @@ import { allAppRoutes } from './routes';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     RouterModule.forRoot(allAppRoutes)
   ],
   providers: [],
