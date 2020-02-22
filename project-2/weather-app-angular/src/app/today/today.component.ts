c6e07f055bbbc54b7bd31ab408ca214898fa61e5
glaucia86 <glaucia_lemos86@hotmail.com>
2020-02-22 03:02:47 -0300

    Incluindos as imagens para o projeto.

--- project-2/weather-app-angular/src/app/today/today.component.ts
+++ project-2/weather-app-angular/src/app/today/today.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-today',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodayComponent implements OnInit {
 
-  constructor() { }
+  lat;
+  lon;
+  weather;
+
+  constructor(private weatherService: WeatherService) { }
 
   ngOnInit() {
+    this.getLocation();
   }
 
+  // ==> Método responsável por indicar a geolocalização pelo browser:
+  getLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.watchPosition((success) => {
+        this.lat = success.coords.latitude;
+        this.lon = success.coords.longitude;
+
+        this.weatherService.getWeatherDataByCoords(this.lat, this.lon).subscribe(data => {
+          this.weather = data;
+
+          console.log(data);
+        });
+      });
+    }
+  }
 }
