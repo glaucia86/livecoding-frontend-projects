718233483ce8908a2ec2cb174d811e32b41cdafa
glaucia86 <glaucia_lemos86@hotmail.com>
2020-02-22 02:07:14 -0300

    Desenvolvimento do arquivo: 'weather.service.ts'

--- project-2/weather-app-angular/src/app/weather.service.ts
+++ project-2/weather-app-angular/src/app/weather.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  url = 'https://api.openweathermap.org/data/2.5/weather';
+  apiKey = '57e0b31962ae34800b8c4142095d57fa';
+
+  constructor(private http: HttpClient) { }
+
+  // ==> Método para retornar os params de 'latitude' e 'longitude' da api:
+  getWeatherDataByCoords(lat, lon) {
+    const params = new HttpParams()
+
+      .set('lat', lat)
+      .set('lon', lon)
+      .set('units', 'metrics')
+      .set('appid', this.apiKey);
+
+    return this.http.get(this.url, { params });
+  }
 }
