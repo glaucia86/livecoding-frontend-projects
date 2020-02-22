2508e299684a3843edadd9af6b2ac8bf3fe167a0
glaucia86 <glaucia_lemos86@hotmail.com>
2020-02-22 01:57:33 -0300

    Configurando a aplicação no arquivo 'app.module.ts'

--- project-2/weather-app-angular/src/app/app.module.ts
+++ project-2/weather-app-angular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TodayComponent } from './today/today.component';
 
@@ -10,7 +11,8 @@ import { TodayComponent } from './today/today.component';
     TodayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
