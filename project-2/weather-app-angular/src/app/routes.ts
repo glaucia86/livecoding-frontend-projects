1f31db42fd4930dd65641cf1ff6906028565a333
glaucia86 <glaucia_lemos86@hotmail.com>
2020-02-22 01:34:22 -0300

    Removendo projeto iniciado.

--- project-2/weather-app-angular/src/app/routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { WeatherComponent } from './weather/weather.component';
-
-export const allAppRoutes: Routes = [
-  { path: '', component: WeatherComponent }
-];
