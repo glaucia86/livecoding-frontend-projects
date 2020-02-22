22fe0c20389ef3e173581309416cbf2ad3c9a805
glaucia86 <glaucia_lemos86@hotmail.com>
2020-02-22 01:53:44 -0300

    Criando os componentes e o service.

---
+++ project-2/weather-app-angular/src/app/today/today.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-today',
+  templateUrl: './today.component.html',
+  styleUrls: ['./today.component.css']
+})
+export class TodayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
